67978571ee54304f96157c16d1c493121914db92
sampon <slamet@pon.co.id>
2020-10-08 19:02:38 +0700

    integrate all base.htm

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,6 +1,6 @@
 # accounts/forms.py
 from django import forms
-from django.forms import widgets
+from django.forms import widgets, ModelForm
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .models import SmansaUser
 from crispy_forms.helper import FormHelper
@@ -8,6 +8,50 @@ from crispy_forms.layout import Layout, Submit, Fieldset, Field
 
 class SmansaUserCreationForm(UserCreationForm):
     password = forms.CharField(widget=forms.PasswordInput())
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.layout = Layout(
+            Fieldset('Entry Data User',
+            'username',
+            'mobile_number',
+            Field('address', css_class='form-group col-md-6 mb-0'),
+            'password',
+            ),
+            Submit('submit', 'Sign in')
+        )
+
+    class Meta:
+        model = SmansaUser
+        fields = ('username', 'mobile_number', 'address', 'password')
+
+class SmansaUserCreate_form(ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+
+    def clean_username(self):
+        data = self.cleaned_data['username']
+        
+        # Remember to always return the cleaned data.
+        return data
+
+    def clean_mobile_number(self):
+        data = self.cleaned_data['mobile_number']
+        
+        # Remember to always return the cleaned data.
+        return data
+
+    def clean_address(self):
+        data = self.cleaned_data['address']
+        
+        # Remember to always return the cleaned data.
+        return data
+
+    def clean_password(self):
+        data = self.cleaned_data['password']
+        
+        # Remember to always return the cleaned data.
+        return data
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
--- accounts/views.py
+++ accounts/views.py
@@ -2,17 +2,17 @@
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
 
-from .forms import SmansaUserCreationForm
+from .forms import SmansaUserCreate_form
 
 class SignUpView(CreateView):
-    form_class = SmansaUserCreationForm
+    form_class = SmansaUserCreate_form
     success_url = reverse_lazy('login')
     template_name = 'registration/signup.html'
 
     def form_valid(self, form,**kwargs):
         self.object = form.save(commit=False)
 
-        print('SignUpView')
+        self.object.set_password(form.cleaned_data['password'])
 
         self.object.save()
 
